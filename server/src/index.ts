e4f993b0bf089dfcebc88ce63952724626f04a48
kenbomtom <kenbomtom@gmail.com>
2022-03-12 02:00:13 +0000

    add apollo server difinition

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,15 @@
-const age: number = 2412233
-console.log(age)
+import { ApolloServer } from "apollo-server"
+import { typeDefs } from './schema'
+import { Query } from "./resolvers"
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers:{
+        Query , 
+    }
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready on ${url}`)
+})
