4a2f5c48bc2900f39133a8ab2e1119dac0a2992a
kenbomtom <kenbomtom@gmail.com>
2022-04-07 03:25:06 +0000

    just start adding shiwake

--- server/src/resolvers/Mutation/shiwake.ts
+++ server/src/resolvers/Mutation/shiwake.ts
@@ -1,3 +1,41 @@
+import { User } from ".prisma/client"
+import { Context } from "../../index"
+
+
+interface ShiwakeCreateArgs {
+    input: {
+        userName: string
+        mail: string
+    }
+}
+
+interface ShiwakePayloadType {
+    userErrors: {
+        message: String
+    }[],
+    user: User | null
+}
+
 export const shiwakeRosolver = {
-    
+    userCreate: async (parent: any, { input }: ShiwakeCreateArgs, { prisma }: Context): Promise<ShiwakePayloadType> => {
+        const { userName, mail } = input
+
+        if (!userName || !mail)
+            return {
+                userErrors: [{ message: "You must provide both userName and mail." }],
+                user: null
+            }
+
+        const user = await prisma.user.create({
+            data: {
+                userName,
+                mail,
+            }
+        })
+        return {
+            userErrors: [],
+            user: user
+
+        }
+    } 
 }
