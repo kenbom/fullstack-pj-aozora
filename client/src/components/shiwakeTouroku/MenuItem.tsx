46f5989dd277b239001842e43c17185a59de10bc
kenbom <tsutomu.kimura@nifty.ne.jp>
2022-04-21 02:05:01 +0000

    modify menu

--- client/src/components/shiwakeTouroku/MenuItem.tsx
+++ client/src/components/shiwakeTouroku/MenuItem.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { menuItems } from "../../config/dataMenuItems";
 
 export const MenuItem = () => {
+  const menu = menuItems;
   return (
     <div>
       <p>Test</p>
+      {menu.map((item) => {
+        return <p key={item.kamokuCd}> {item.kamokuMei}</p>;
+      })}
       <p>Test</p>
       <p>Test</p>
     </div>
   );
-}
+};
