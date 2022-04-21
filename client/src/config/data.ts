96d6b65651b57e3df69f816ee03b6ca0eab2e1b0
kenbomtom <kenbomtom@gmail.com>
2022-04-21 01:21:24 +0000

    add datafile

---
+++ client/src/config/data.ts
@@ -0,0 +1,17 @@
+export const menuItems = [
+    {
+        kamokuGrpCd: 5,
+        kamokuCd: 502,
+        kamokuMei: "仕入"
+    },
+    {
+        kamokuGrpCd: 5,
+        kamokuCd: 503,
+        kamokuMei: "租税公課"
+    },
+    {
+        kamokuGrpCd: 5,
+        kamokuCd: 504,
+        kamokuMei: "荷造運賃"
+    },
+]
