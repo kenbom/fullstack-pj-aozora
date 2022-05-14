f988e8bdd6149b38d2662ae85958992c9a3e5e9a
kenbom <tsutomu.kimura@nifty.ne.jp>
2022-05-14 08:05:38 +0000

    test other browser

--- client/src/components/shiwakeTouroku/hooks/useShiwakeTouroku.tsx
+++ client/src/components/shiwakeTouroku/hooks/useShiwakeTouroku.tsx
@@ -9,16 +9,21 @@ import { useCustomToast } from '../../app/hooks/useCustomToast';
 // import {useCustomToast} from ""
 import type { ShiwakeInput } from "../ShiwakeTouroku";
 import type { Shiwakes } from "../../seisanHyou/hooks/useSeisanHyou";
-import { queryKeys } from "../../../config/queryKeys"
+import { queryKeys } from "../../../config/queryKeys" 
 
 async function setSeisanHyou(input: ShiwakeInput) {
+  localStorage.clear()
   console.log(input);
   const endpoint = BASE_URL;
+  // localStorage.saveKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzLCJpYXQiOjE2NTI0MDAwNTYsImV4cCI6MTY1NjAwMDA1Nn0.2hY4gpW4C-4tZeXZBG7j1JEW67FOymHlL2NcoP2zKmc";
+  localStorage.saveKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI1LCJpYXQiOjE2NTIxNTQ0MzUsImV4cCI6MTY1NTc1NDQzNX0.nAkGAiHpP9ZH80zqeTHm-Kmpq9QGo2QH2aVo8iNy9uM";
+
+  const auth = localStorage.saveKey
   const client = new GraphQLClient(endpoint, {
     headers: {
-      authorization:
+      authorization: auth
         // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI1LCJpYXQiOjE2NTIxNTQ0MzUsImV4cCI6MTY1NTc1NDQzNX0.nAkGAiHpP9ZH80zqeTHm-Kmpq9QGo2QH2aVo8iNy9uM",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzLCJpYXQiOjE2NTI0MDAwNTYsImV4cCI6MTY1NjAwMDA1Nn0.2hY4gpW4C-4tZeXZBG7j1JEW67FOymHlL2NcoP2zKmc"
+        // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzLCJpYXQiOjE2NTI0MDAwNTYsImV4cCI6MTY1NjAwMDA1Nn0.2hY4gpW4C-4tZeXZBG7j1JEW67FOymHlL2NcoP2zKmc"
     },
   });
   const mutation = gql`
@@ -32,9 +37,9 @@ async function setSeisanHyou(input: ShiwakeInput) {
   }                          
   `;
   const requestHeaders = {
-    authorization:
+    authorization:auth
     // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI1LCJpYXQiOjE2NTIxNTQ0MzUsImV4cCI6MTY1NTc1NDQzNX0.nAkGAiHpP9ZH80zqeTHm-Kmpq9QGo2QH2aVo8iNy9uM"
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzLCJpYXQiOjE2NTI0MDAwNTYsImV4cCI6MTY1NjAwMDA1Nn0.2hY4gpW4C-4tZeXZBG7j1JEW67FOymHlL2NcoP2zKmc"
+    // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzLCJpYXQiOjE2NTI0MDAwNTYsImV4cCI6MTY1NjAwMDA1Nn0.2hY4gpW4C-4tZeXZBG7j1JEW67FOymHlL2NcoP2zKmc"
   }
   const data = await client.request(mutation, input, requestHeaders);
   console.log(`returnedData:${JSON.stringify(data)}`)
