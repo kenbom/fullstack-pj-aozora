34cadc13a96bfb1393db4c8e1ef61bd4b0bc0cf6
kenbom <tsutomu.kimura@nifty.ne.jp>
2022-05-16 20:56:27 +0000

    made signin form

---
+++ client/src/components/auth/AuthHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Box} from '@chakra-ui/react'
+
+export const AuthHeader = () => {
+  return (
+    <Box borderBottom="2px" borderBottomColor="blue.300" mb={10}>
+      らくらく！青色申告
+    </Box>
+  );
+}
--- client/src/components/auth/Signin.tsx
+++ client/src/components/auth/Signin.tsx
@@ -1,4 +1,5 @@
-import React, { VFC } from 'react'
+import React, { VFC } from "react";
+import { AuthHeader } from "./AuthHeader";
 import {
   FormControl,
   FormLabel,
@@ -8,74 +9,110 @@ import {
   Input,
   Box,
   Center,
+  HStack,
+  Spacer,
+  Flex,
 } from "@chakra-ui/react";
-import { Formik, Form, Field, } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import {
   InputControl,
   PercentComplete,
   ResetButton,
   SubmitButton,
-  CheckboxSingleControl
-} from "formik-chakra-ui"
-import { GraphQLEnumType } from 'graphql';
+  CheckboxSingleControl,
+} from "formik-chakra-ui";
+import { GraphQLEnumType } from "graphql";
 
 const SignupSchema = Yup.object().shape({
   password: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+    .matches(/^[a-zA-Z0-9!-/:-@¥[-`{-~]*$/, "英数字半角にて設定してください")
+    .min(2, "4文字以上で設定してください")
+    .max(50, "Too Long!")
+    .required("必須項目です"),
   // lastName: Yup.string()
   //   .min(2, 'Too Short!')
   //   .max(50, 'Too Long!')
   //   .required('Required'),
-  email: Yup.string().email('正しいメールアドレスを入力してください').required('メールアドレスは必須です'),
+  email: Yup.string()
+    .matches(/^[a-zA-Z0-9!-/:-@¥[-`{-~]*$/, "英数字半角にて入力してください")
+    .email("正しいメールアドレスを入力してください")
+    .required("必須項目です"),
 });
 
 export const Signin: VFC = () => {
   return (
-    <Box >
-      <Center>
-        <Box w="300px" p="20px" border="1px" bgColor="white" mt='50px'>
-          <Formik initialValues={{
-            email: '',
-            password: '',
-          }}
-            validationSchema={SignupSchema}
-            onSubmit={values => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null , `  `))
-              }, 1000)
-            }}
-          >
-            {({ errors, touched }) => (
-              <Form>
-                {/* <Field name="firstName" />
+    <>
+      <Box>
+        <Center>
+          <Box w="300px" p="20px" border="1px" bgColor="white" mt="50px">
+            <AuthHeader></AuthHeader>
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={SignupSchema}
+              onSubmit={(values) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, `  `));
+                }, 1000);
+              }}
+              onReset={(values) => {
+                values.email = "";
+                values.password = "";
+              }}
+            >
+              {({ errors, touched }) => (
+                <Form>
+                  {/* <Field name="firstName" />
                 {({ field, form }) => (
               <FormControl isInvalid={form.errors.name && form.touched.name}>
                 <FormLabel htmlFor='firstName'>First name</FormLabel> */}
-                <InputControl name='email' label='メールアドレス' />
-                {/* <FormErrorMessage>{form.errors.firstName}</FormErrorMessage> */}
-              {/* </FormControl> */}
-            {/* )} */}
-                {/* {errors.firstName && touched.firstName ? (
+                  <InputControl name="email" label="メールアドレス" mb={10} />
+                  {/* <FormErrorMessage>{form.errors.firstName}</FormErrorMessage> */}
+                  {/* </FormControl> */}
+                  {/* )} */}
+                  {/* {errors.firstName && touched.firstName ? (
                   <div>{errors.firstName}</div>
                 ) : null} */}
-                {/* <Field name="lastName" />
+                  {/* <Field name="lastName" />
                 {errors.lastName && touched.lastName ? (
                   <div>{errors.lastName}</div>
                 ) : null}
                 <Field name="email" type="email" />
                 {errors.email && touched.email ? <div>{errors.email}</div> : null} */}
-                <InputControl name='password' label='パスワード' />
-                <SubmitButton mt={4} color="gray.800" bgColor="gray.200"
-                  // colorScheme='blue'
-                  type='submit'>Signin</SubmitButton>
-              </Form>
-            )}
-          </Formik>
-        </Box>
-      </Center>
-    </Box>
+                  <InputControl name="password" label="パスワード" mb={5} />
+                  <Flex>
+                    <SubmitButton
+                      mt={4}
+                      h={12}
+                      //   color="gray.800"
+                      //   bgColor="gray.200"
+                      colorScheme="gray"
+                      type="submit"
+                    >
+                      Signin
+                    </SubmitButton>
+                    <Spacer />
+                    <ResetButton
+                      mt={4}
+                      h={10}
+                      pt={6}
+                      pb={6} //   color="black"
+                      //   bgColor="gray.200"
+                      colorScheme="gray"
+                      type="reset"
+                    >
+                      Reset
+                    </ResetButton>
+                  </Flex>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+        </Center>
+      </Box>
+    </>
   );
-}
+};
